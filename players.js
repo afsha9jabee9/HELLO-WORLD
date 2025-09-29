08bc14daf206b82bc6377ffdf1db89f0682796cc
afsha9jabee9 <afsha9jabee9@gmail.com>
2025-09-28 22:53:15 -0500

    Update players.js

--- players.js
+++ players.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Player = require("../models/Player");
 
-// test GET route
-router.get("/", (req, res) => {
-  res.send("sending all players");
+// Get all players
+router.get("/", async (req, res) => {
+  try {
+    const players = await Player.find();
+    res.json(players);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Add a new player
+router.post("/", async (req, res) => {
+  const player = new Player(req.body);
+  try {
+    const savedPlayer = await player.save();
+    res.status(201).json(savedPlayer);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
